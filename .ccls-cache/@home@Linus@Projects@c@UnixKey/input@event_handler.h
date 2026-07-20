965d49edceb984ea8221a8c369b81655688457f0
Linus <linus.schneeberg@gmail.com>
2026-07-20 02:03:30 +0200

    remove ccls cache

--- .ccls-cache/@home@Linus@Projects@c@UnixKey/input@event_handler.h
+++
@@ -1,9 +0,0 @@
-#include <fcntl.h>
-#include <linux/input.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/select.h>
-#include <unistd.h>
-
-
-void start_listening(int device);
--- .ccls-cache/@home@Linus@Projects@c@UnixKey/logic@word_state.h
+++
@@ -1,1 +0,0 @@
-// Will include a buffer for the current word (raw typing), the current word on screen (word with substitutions) and the word that was on screen before the last substitution (for undoing)
