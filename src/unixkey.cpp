f967ff7b3cbe0aebf5a841df74b395384526a4dc
Linus <linus.schneeberg@gmail.com>
2026-07-22 03:06:06 +0200

    Implement kind of working Quwei input heavily based on fcitx-quwei

--- src/unixkey.cpp
+++ src/unixkey.cpp
@@ -1,15 +1,310 @@
 #include "unixkey.h"
+#include "fcitx-utils/keysym.h"
+#include <Fcitx5/Module/fcitx-module/punctuation/punctuation_public.h>
+#include <Fcitx5/Module/fcitx-module/quickphrase/quickphrase_public.h>
+#include <algorithm>
+#include <array>
+#include <fcitx-utils/capabilityflags.h>
+#include <fcitx-utils/cutf8.h>
 #include <fcitx-utils/key.h>
 #include <fcitx-utils/log.h>
 #include <fcitx-utils/macros.h>
+#include <fcitx-utils/textformatflags.h>
+#include <fcitx-utils/utf8.h>
 #include <fcitx/addoninstance.h>
+#include <fcitx/candidatelist.h>
 #include <fcitx/event.h>
+#include <fcitx/inputcontext.h>
 #include <fcitx/inputmethodentry.h>
+#include <fcitx/inputpanel.h>
+#include <fcitx/instance.h>
+#include <fcitx/statusarea.h>
+#include <fcitx/text.h>
+#include <fcitx/userinterface.h>
+#include <fcitx/userinterfacemanager.h>
+#include <iconv.h>
+#include <memory>
+#include <stdexcept>
+#include <string>
 
-void QuweiEngine::keyEvent(const fcitx::InputMethodEntry &entry,
-                           fcitx::KeyEvent &keyEvent) {
-  FCITX_UNUSED(entry);
-  FCITX_INFO() << keyEvent.key() << "isRelease=" << keyEvent.isRelease();
+namespace {
+static const std::array<fcitx::Key, 10> selectionKeys = {
+    fcitx::Key{FcitxKey_1}, fcitx::Key{FcitxKey_2}, fcitx::Key{FcitxKey_3},
+    fcitx::Key{FcitxKey_4}, fcitx::Key{FcitxKey_5}, fcitx::Key{FcitxKey_6},
+    fcitx::Key{FcitxKey_7}, fcitx::Key{FcitxKey_8}, fcitx::Key{FcitxKey_9},
+    fcitx::Key{FcitxKey_0},
+};
+
+class QuweiCandidateWord : public fcitx::CandidateWord {
+public:
+  QuweiCandidateWord(UnixKeyEngine *engine, std::string text)
+      : engine_(engine) {
+    setText(fcitx::Text(std::move(text)));
+  }
+
+  void select(fcitx::InputContext *inputContext) const override {
+    inputContext->commitString(text().toString());
+    auto state = inputContext->propertyFor(engine_->factory());
+    state->reset();
+  }
+
+private:
+  UnixKeyEngine *engine_;
+};
+
+class QuweiCandidateList : public fcitx::CandidateList,
+                           public fcitx::PageableCandidateList,
+                           public fcitx::CursorMovableCandidateList {
+public:
+  QuweiCandidateList(UnixKeyEngine *engine, fcitx::InputContext *ic,
+                     const std::string &code)
+      : engine_(engine), ic_(ic), code_(std::stoi(code)) {
+    setPageable(this);
+    setCursorMovable(this);
+    for (int i = 0; i < 10; i++) {
+      const char label[2] = {static_cast<char>('0' + (i + 1) % 10), '\0'};
+      labels_[i].append(label);
+      labels_[i].append(". ");
+    }
+    generate();
+  }
+
+  const fcitx::Text &label(int idx) const override { return labels_[idx]; }
+
+  const fcitx::CandidateWord &candidate(int idx) const override {
+    return *candidates_[idx];
+  }
+  int size() const override { return 10; }
+  fcitx::CandidateLayoutHint layoutHint() const override {
+    return fcitx::CandidateLayoutHint::NotSet;
+  }
+  bool usedNextBefore() const override { return false; }
+  void prev() override {
+    if (!hasPrev()) {
+      return;
+    }
+    code_--;
+    auto state = ic_->propertyFor(engine_->factory());
+    state->setCode(code_);
+  }
+
+  void next() override {
+    if (!hasNext()) {
+      return;
+    }
+    code_++;
+    auto state = ic_->propertyFor(engine_->factory());
+    state->setCode(code_);
+  }
+
+  bool hasPrev() const override { return code_ > 0; }
+
+  bool hasNext() const override { return code_ < 999; }
+
+  void prevCandidate() override { cursor_ = (cursor_ + 9) % 10; }
+
+  void nextCandidate() override { cursor_ = (cursor_ + 1) % 10; }
+
+  int cursorIndex() const override { return cursor_; }
+
+private:
+  void generate() {
+    for (int i = 0; i < 10; i++) {
+      auto code = code_ * 10 + (i + 1);
+      auto qu = code / 100;
+      auto wei = code % 100;
+
+      char in[3];
+      if (qu >= 95) {
+        in[0] = qu - 95 + 0xA8;
+        in[1] = wei + 0x40;
+
+        if (in[1] >= 0x7f) {
+          in[1]++;
+        }
+      } else {
+        in[0] = qu + 0xa0;
+        in[1] = wei + 0xa0;
+      }
+
+      size_t insize = 2, avail = FCITX_UTF8_MAX_LENGTH + 1;
+      char *inbuf = in;
+
+      char out[FCITX_UTF8_MAX_LENGTH + 1];
+      char *outbuf = out;
+      iconv(engine_->conv(), &inbuf, &insize, &outbuf, &avail);
+      ;
+    }
+  }
+
+  UnixKeyEngine *engine_;
+  fcitx::InputContext *ic_;
+  fcitx::Text labels_[10];
+  std::unique_ptr<QuweiCandidateWord> candidates_[10];
+  int code_;
+  int cursor_ = 0;
+};
+
+} // namespace
+
+void QuweiState::keyEvent(fcitx::KeyEvent &event) {
+  if (auto candidateList = ic_->inputPanel().candidateList()) {
+    int idx = event.key().keyListIndex(selectionKeys);
+    if (idx >= 0 && idx < candidateList->size()) {
+      event.accept();
+      candidateList->candidate(idx).select(ic_);
+      return;
+    }
+    if (event.key().checkKeyList(
+            engine_->instance()->globalConfig().defaultPrevPage())) {
+      if (auto *pageable = candidateList->toPageable();
+          pageable && pageable->hasPrev()) {
+        event.accept();
+        pageable->prev();
+        ic_->updateUserInterface(fcitx::UserInterfaceComponent::InputPanel);
+      }
+      return event.filterAndAccept();
+    }
+
+    if (event.key().checkKeyList(
+            engine_->instance()->globalConfig().defaultNextPage())) {
+      if (auto *pageable = candidateList->toPageable();
+          pageable && pageable->hasNext()) {
+        pageable->next();
+        ic_->updateUserInterface(fcitx::UserInterfaceComponent::InputPanel);
+      }
+      return event.filterAndAccept();
+    }
+  }
+
+  if (buffer_.empty()) {
+    if (!event.key().isDigit()) {
+      auto c = fcitx::Key::keySymToUnicode(event.key().sym());
+      if (!c) {
+        return;
+      }
+      std::string punc, puncAfter;
+      if (c && !event.key().isKeyPad()) {
+        std::tie(punc, puncAfter) =
+            engine_->punctuation()
+                ->call<fcitx::IPunctuation::pushPunctuationV2>("zh_CN", ic_, c);
+      }
+      if (event.key().check(FcitxKey_semicolon) && engine_->quickphrase()) {
+        auto keyString = fcitx::utf8::UCS4ToUTF8(c);
+        auto output = !punc.empty() ? (punc + puncAfter) : keyString;
+        auto altOutput = !punc.empty() ? keyString : "";
+        std::string text;
+        engine_->quickphrase()->call<fcitx::IQuickPhrase::trigger>(
+            ic_, text, "", output, altOutput, fcitx::Key(FcitxKey_semicolon));
+        event.filterAndAccept();
+        return;
+      }
+      if (!punc.empty()) {
+        event.filterAndAccept();
+        ic_->commitString(punc + puncAfter);
+        if (size_t length = fcitx::utf8::lengthValidated(puncAfter);
+            length != 0 && length != fcitx::utf8::INVALID_LENGTH) {
+          for (size_t i = 0; i < length; i++) {
+            ic_->forwardKey(fcitx::Key(FcitxKey_Left));
+          }
+        }
+      }
+      return;
+    }
+    // buffer not empty
+  } else {
+    if (event.key().check(FcitxKey_BackSpace)) {
+      buffer_.backspace();
+      updateUI();
+      return event.filterAndAccept();
+    }
+    if (event.key().check(FcitxKey_Return)) {
+      ic_->commitString(buffer_.userInput());
+      reset();
+      return event.filterAndAccept();
+    }
+    if (event.key().check(FcitxKey_Escape)) {
+      reset();
+      return event.filterAndAccept();
+    }
+    if (!event.key().isDigit()) {
+      return event.filterAndAccept();
+    }
+  }
+
+  if (event.key().isDigit()) {
+    buffer_.type('0' + event.key().digit());
+  }
+  updateUI();
+  return event.filterAndAccept();
 }
 
-FCITX_ADDON_FACTORY(QuweiEngineFactory)
+void QuweiState::setCode(int code) {
+  if (code < 0 || code > 999) {
+    return;
+  }
+  buffer_.clear();
+  auto codeStr = std::to_string(code);
+  while (codeStr.size() < 3) {
+    codeStr = "0" + codeStr;
+  }
+  buffer_.type(std::to_string(code));
+  updateUI();
+}
+
+void QuweiState::updateUI() {
+  auto &inputPanel = ic_->inputPanel();
+  inputPanel.reset();
+  if (buffer_.size() == 3) {
+    inputPanel.setCandidateList(std::make_unique<QuweiCandidateList>(
+        engine_, ic_, buffer_.userInput()));
+  }
+  if (ic_->capabilityFlags().test(fcitx::CapabilityFlag::Preedit)) {
+    fcitx::Text preedit(buffer_.userInput(), fcitx::TextFormatFlag::HighLight);
+    inputPanel.setClientPreedit(preedit);
+  }
+}
+
+UnixKeyEngine::UnixKeyEngine(fcitx::Instance *instance)
+    : instance_(instance), factory_([this](fcitx::InputContext &ic) {
+        return new QuweiState(this, &ic);
+      }) {
+  conv_ = iconv_open("UTF-8", "GB18030");
+  if (conv_ == reinterpret_cast<iconv_t>(-1)) {
+    throw std::runtime_error("Failed to create converter");
+  }
+  instance->inputContextManager().registerProperty("quweiState", &factory_);
+}
+
+void UnixKeyEngine::activate(const fcitx::InputMethodEntry &entry,
+                             fcitx::InputContextEvent &event) {
+  FCITX_UNUSED(entry);
+  auto *inputContext = event.inputContext();
+  fullwidth();
+  chttrans();
+  for (const auto *actionName : {"chttrans", "punctuation", "fullwidth"}) {
+    if (auto *action =
+            instance_->userInterfaceManager().lookupAction(actionName)) {
+      inputContext->statusArea().addAction(fcitx::StatusGroup::InputMethod,
+                                           action);
+    }
+  }
+}
+
+void UnixKeyEngine::keyEvent(const fcitx::InputMethodEntry &entry,
+                             fcitx::KeyEvent &keyEvent) {
+  FCITX_UNUSED(entry);
+  if (keyEvent.isRelease() || keyEvent.key().states()) {
+    return;
+  }
+  auto ic = keyEvent.inputContext();
+  auto *state = ic->propertyFor(&factory_);
+  state->keyEvent(keyEvent);
+}
+
+void UnixKeyEngine::reset(const fcitx::InputMethodEntry &, fcitx::InputContextEvent &event) {
+  auto *state = event.inputContext()->propertyFor(&factory_);
+  state->reset();
+}
+
+FCITX_ADDON_FACTORY(UnixKeyEngineFactory)
--- src/unixkey.h
+++ src/unixkey.h
@@ -1,19 +1,72 @@
-#ifndef _FCITX5_QUWEI_QUWEI_H_
-#define _FCITX5_QUWEI_QUWEI_H_
+#ifndef _FCITX5_UNIXKEY_H_
+#define _FCITX5_UNIXKEY_H_
 
+#include <fcitx-utils/inputbuffer.h>
 #include <fcitx/addonfactory.h>
+#include <fcitx/addoninstance.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/event.h>
+#include <fcitx/inputcontext.h>
+#include <fcitx/inputcontextproperty.h>
 #include <fcitx/inputmethodengine.h>
+#include <fcitx/inputmethodentry.h>
+#include <fcitx/instance.h>
+#include <iconv.h>
 
-class QuweiEngine : public fcitx::InputMethodEngineV2 {
-  void keyEvent(const fcitx::InputMethodEntry &entry,
-                fcitx::KeyEvent &keyEvent) override;
+class UnixKeyEngine;
+
+class QuweiState : public fcitx::InputContextProperty {
+public:
+  QuweiState(UnixKeyEngine *engine, fcitx::InputContext *ic)
+      : engine_(engine), ic_(ic) {}
+
+  void keyEvent(fcitx::KeyEvent &keyEvent);
+  void setCode(int code);
+  void updateUI();
+  void reset() {
+    buffer_.clear();
+    updateUI();
+  }
+
+private:
+  UnixKeyEngine *engine_;
+  fcitx::InputContext *ic_;
+  fcitx::InputBuffer buffer_{{fcitx::InputBufferOption::AsciiOnly,
+                              fcitx::InputBufferOption::FixedCursor}};
 };
 
-class QuweiEngineFactory : public fcitx::AddonFactory {
+class UnixKeyEngine : public fcitx::InputMethodEngineV2 {
+public:
+  UnixKeyEngine(fcitx::Instance *instance);
+
+  void activate(const fcitx::InputMethodEntry &entry,
+                fcitx::InputContextEvent &event) override;
+  void keyEvent(const fcitx::InputMethodEntry &entry,
+                fcitx::KeyEvent &keyEvent) override;
+
+  void reset(const fcitx::InputMethodEntry &,
+             fcitx::InputContextEvent &event) override;
+
+  auto factory() const { return &factory_; }
+  auto conv() const { return conv_; }
+  auto instance() const { return instance_; }
+
+  FCITX_ADDON_DEPENDENCY_LOADER(quickphrase, instance_->addonManager());
+  FCITX_ADDON_DEPENDENCY_LOADER(punctuation, instance_->addonManager());
+
+private:
+  FCITX_ADDON_DEPENDENCY_LOADER(chttrans, instance_->addonManager());
+  FCITX_ADDON_DEPENDENCY_LOADER(fullwidth, instance_->addonManager());
+
+  fcitx::Instance *instance_;
+  fcitx::FactoryFor<QuweiState> factory_;
+  iconv_t conv_;
+};
+
+class UnixKeyEngineFactory : public fcitx::AddonFactory {
   fcitx::AddonInstance *create(fcitx::AddonManager *manager) override {
-    FCITX_UNUSED(manager);
-    return new QuweiEngine;
+    return new UnixKeyEngine(manager->instance());
   }
 };
 
-#endif // _FCITX5_QUWEI_QUWEI_H_
+#endif // _FCITX5_UNIXKEY_H_
